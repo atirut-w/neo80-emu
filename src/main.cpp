0eac5dd4d1a2d49159330a2a319095dea90052e2
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-05-15 19:53:41 +0700

    Stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
 
-int main()
+using namespace argparse;
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    ArgumentParser program("neoz-emu");
+
+    program.add_argument("-r", "--rom")
+        .help("path to the ROM file");
+    
+    program.add_argument("-m", "--memory")
+        .help("amount of memory available in units of 4KiB banks");
+    
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+
+    if (program["--help"] == true)
+    {
+        std::cout << program;
+        exit(0);
+    }
 
     return 0;
 }
